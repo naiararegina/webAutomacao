37ed6ba2e8bd1fc80387a34a71961da0902c9979
naiararegina <naiararegina54@gmail.com>
2020-07-13 22:09:16 -0300

    Atualizando

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -8,19 +8,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Random;
 
 public class Selenium {
     //cria instancia do drive chrome
-    private static WebDriver driver = new ChromeDriver();
-    private WebDriverWait wait = new WebDriverWait(driver, 100);
+    private static WebDriver driver;
+    private static WebDriverWait wait;
 
     @BeforeAll
     public static void setup(){
         //setar as propriedade do chrome drive
-        System.setProperty( "webdriver.chrome.driver" , "./src/main/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver" , "./src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 100);
 
         // abrir o browser pe acessar a url
         driver.get("https://automacaocombatista.herokuapp.com");
@@ -44,6 +47,7 @@ public class Selenium {
 
     //Identificar um elemento preenchendo o campo com um texto
         driver.findElement(By.xpath("//a[text()='Formulário']")).click();
+
         driver.findElement(By.xpath("//a[text()='Criar Usuários']")).click();
         driver.findElement(By.id("user_name")).sendKeys("Naiara"); //1° encontra um elemento na tela 2° inserir texto no campo
         driver.findElement(By.id("user_lastname")).sendKeys("Silva");
@@ -61,10 +65,7 @@ public class Selenium {
 
         //validando se a mensagem que eu necessito esta aparecendo na tela
         Assertions.assertEquals("Usuário Criado com sucesso", mensagem);
-
-
     }
-
 
 
     @Test
@@ -73,16 +74,47 @@ public class Selenium {
 
         WebElement link = driver.findElement(By.xpath("//a[text()='Radio e Checkbox']"));
         wait.until(ExpectedConditions.visibilityOf(link));
+        link.click();
 
-        driver.findElement(By.xpath("//input[@id='red']")).click();
+        /*driver.findElement(By.xpath("//input[@id='red']")).click();
         driver.findElement(By.xpath("//input[@id='blue']")).click();
         driver.findElement(By.xpath("//input[@id='yellow']")).click();
         driver.findElement(By.xpath("//input[@id='green']")).click();
         driver.findElement(By.xpath("//input[@id='purple']")).click();
         driver.findElement(By.xpath("//input[@id='grey']")).click();
         driver.findElement(By.xpath("//input[@id='black']")).click();
-        driver.findElement(By.xpath("//input[@id='white']")).click();
+        driver.findElement(By.xpath("//input[@id='white']")).click();*/
 
+
+
+    }
+
+    @Test
+    public void select() {
+        driver.findElement(By.xpath("//a[text()='Busca de elementos']")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Dropdown e Select']")));
+
+        driver.findElement(By.xpath("//a[text()='Dropdown e Select']")).click();
+
+        driver.findElement(By.xpath("//label[text()='Desenho Favorito']/preceding-sibling::div[@class='select-wrapper']")).click();
+        driver.findElement(By.xpath("//span[text()='Dragon Ball']")).click();
+
+        driver.findElement(By.xpath("//span[text()='Ronaldinho Gaucho']/ancestor::ul/preceding-sibling::input")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Ronaldinho Gaucho']")));
+        driver.findElement(By.xpath("//span[text()='Ronaldinho Gaucho']")).click();
+
+
+        WebElement listaSelecao = driver.findElement(By.xpath("//select[@id ='dropdown']"));
+
+        //Existe uma classe só para o select do Selenium
+       Select select = new Select(listaSelecao);
+
+       select.selectByIndex(1);
+
+       //valor que fica dentro do elemento value
+     //  select.selectByValue("3");
+
+     //  select.selectByVisibleText("Internet Explorer");
 
 
     }
